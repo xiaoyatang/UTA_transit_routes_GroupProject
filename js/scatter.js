92196d56853912ee45c13c6fe659d0f6d5b9bdca
Milena Belianovich <112034622+milenabel@users.noreply.github.com>
2022-11-28 16:46:17 -0700

    update

--- js/scatter.js
+++ js/scatter.js
@@ -1,3 +1,20 @@
 class Scatter {
-    
+    constructor(globalApplicationState){
+        //this.data = d3.csv('UTA_Stop_Boardings_-_Bus.csv', function(){});
+        this.globalApplicationState = globalApplicationState;
+        this.
+        this.svg = d3.select('#chart2');
+        this.height = 500;
+        this.width = 700;
+        this.yAxisPadding = 80;
+        this.xAxisPadding = 50;
+
+        // this.xAxis = d3.scale;
+        this.yAxis = d3.scaleLinear()
+            .domain()
+            .range([this.height - this.xAxisPadding, 10])
+            .nice();
+
+
+    }
 }
