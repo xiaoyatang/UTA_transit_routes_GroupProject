659325498107a55da5e20363105b8dc7cd65f81e
Milena Belianovich <112034622+milenabel@users.noreply.github.com>
2022-11-28 16:53:33 -0700

    update script w stop boardings

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ async function loadData () {
     const routesData = await d3.json('data/map.json');
     const stopsData = await d3.json('data/stops.json');
     const busBoardData = await d3.csv('data/UTA_Route_Level_Boardings_Monthly_Counts.csv');
-    return { routesData, stopsData, busBoardData };
+    const stopBoardData = await d3.csv('data/UTA_Stop_Boardings_-_Bus.csv.csv');
+    return { routesData, stopsData, busBoardData, stopBoardData };
   }
   
   
@@ -16,6 +17,7 @@ async function loadData () {
     routesData: null,
     stopsData: null,
     busBoardData: null,
+    stopBoardData : null,
     map: null,
     chart1: null,
     chart2: null,
@@ -27,12 +29,16 @@ async function loadData () {
     //console.log('Here is the imported data:', loadedData.covidData);
     console.log('Routes:', loadedData.routesData);
     console.log('Stops:', loadedData.stopsData);
-    console.log('busBoardData:', loadedData.busBoardData);
+    console.log('Route Boardings:', loadedData.busBoardData);
+    console.log('Stop Boardings:', loadedData.stopBoardData);
+
   
     // Store the loaded data into the globalApplicationState
     globalApplicationState.routesData = loadedData.routesData;
     globalApplicationState.stopsData = loadedData.stopsData;
-    globalApplicationState.busBoardData=loadedData.busBoardData;
+    globalApplicationState.busBoardData = loadedData.busBoardData;
+    globalApplicationState.stopBoardData = loadedData.stopBoardData;
+
   
     // Creates the view objects with the global state passed in 
     const map = new MapVis("data/map.json", "data/stops.json");
