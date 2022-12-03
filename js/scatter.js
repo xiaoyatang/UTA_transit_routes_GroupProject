9c854c8f718e257a572a51c7d3fe24f8d51f3318
Milena Belianovich <112034622+milenabel@users.noreply.github.com>
2022-12-02 19:58:00 -0700

    hidden changes for colorscale

--- js/scatter.js
+++ js/scatter.js
@@ -1,3 +1,4 @@
+// import {Legend, Swatches} from "@d3/color-legend"
 class Scatter {
 
     constructor(globalApplicationState){
@@ -19,6 +20,7 @@ class Scatter {
 
         let year=d3.select('#Year').property('value');
         let dayType = d3.select('#metric2').property('value');
+        // this.colorScale();
         this.setText();
         this.update(year, dayType);
     }
@@ -131,4 +133,122 @@ class Scatter {
             //     .attr("r", 2.5);
             //     });
     }
+    // colorScale(){
+        // console.log(d3.schemeSet3);
+        // console.log(this.colors);
+        // let scale = Legend(d3.scaleOrdinal(["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"], d3.schemeSpectral[13]), {
+        //     title: "Months",
+        //     tickSize: 0
+        // });
+        // d3.select("#colorScale").attr('', scale);
+    // }
+
+    // // Copyright 2021, Observable Inc.
+    // // Released under the ISC license.
+    // // https://observablehq.com/@d3/color-legend
+    // Legend(color, {
+    //     title,
+    //     tickSize = 6,
+    //     width = 320, 
+    //     height = 44 + tickSize,
+    //     marginTop = 18,
+    //     marginRight = 0,
+    //     marginBottom = 16 + tickSize,
+    //     marginLeft = 0,
+    //     ticks = width / 64,
+    //     tickFormat,
+    //     tickValues
+    // } = {}) {
+    
+    //     function ramp(color, n = 256) {
+    //     const canvas = document.createElement("canvas");
+    //     canvas.width = n;
+    //     canvas.height = 1;
+    //     const context = canvas.getContext("2d");
+    //     for (let i = 0; i < n; ++i) {
+    //         context.fillStyle = color(i / (n - 1));
+    //         context.fillRect(i, 0, 1, 1);
+    //     }
+    //     return canvas;
+    //     }
+    
+    //     let svg = d3.create("svg")
+    //         .attr("width", width)
+    //         .attr("height", height)
+    //         .attr("viewBox", [0, 0, width, height])
+    //         .style("overflow", "visible")
+    //         .style("display", "block");
+    
+    //     let tickAdjust = g => g.selectAll(".tick line").attr("y1", marginTop + marginBottom - height);
+    //     let x;
+    
+    //     // Threshold
+    //     else if (color.invertExtent) {
+    //     const thresholds
+    //         = color.thresholds ? color.thresholds() // scaleQuantize
+    //         : color.quantiles ? color.quantiles() // scaleQuantile
+    //         : color.domain(); // scaleThreshold
+    
+    //     const thresholdFormat
+    //         = tickFormat === undefined ? d => d
+    //         : typeof tickFormat === "string" ? d3.format(tickFormat)
+    //         : tickFormat;
+    
+    //     x = d3.scaleLinear()
+    //         .domain([-1, color.range().length - 1])
+    //         .rangeRound([marginLeft, width - marginRight]);
+    
+    //     svg.append("g")
+    //         .selectAll("rect")
+    //         .data(color.range())
+    //         .join("rect")
+    //         .attr("x", (d, i) => x(i - 1))
+    //         .attr("y", marginTop)
+    //         .attr("width", (d, i) => x(i) - x(i - 1))
+    //         .attr("height", height - marginTop - marginBottom)
+    //         .attr("fill", d => d);
+    
+    //     tickValues = d3.range(thresholds.length);
+    //     tickFormat = i => thresholdFormat(thresholds[i], i);
+    //     }
+    
+    //     // Ordinal
+    //     else {
+    //     x = d3.scaleBand()
+    //         .domain(color.domain())
+    //         .rangeRound([marginLeft, width - marginRight]);
+    
+    //     svg.append("g")
+    //         .selectAll("rect")
+    //         .data(color.domain())
+    //         .join("rect")
+    //         .attr("x", x)
+    //         .attr("y", marginTop)
+    //         .attr("width", Math.max(0, x.bandwidth() - 1))
+    //         .attr("height", height - marginTop - marginBottom)
+    //         .attr("fill", color);
+    
+    //     tickAdjust = () => {};
+    //     }
+    
+    //     svg.append("g")
+    //         .attr("transform", `translate(0,${height - marginBottom})`)
+    //         .call(d3.axisBottom(x)
+    //         .ticks(ticks, typeof tickFormat === "string" ? tickFormat : undefined)
+    //         .tickFormat(typeof tickFormat === "function" ? tickFormat : undefined)
+    //         .tickSize(tickSize)
+    //         .tickValues(tickValues))
+    //         .call(tickAdjust)
+    //         .call(g => g.select(".domain").remove())
+    //         .call(g => g.append("text")
+    //         .attr("x", marginLeft)
+    //         .attr("y", marginTop + marginBottom - height - 6)
+    //         .attr("fill", "currentColor")
+    //         .attr("text-anchor", "start")
+    //         .attr("font-weight", "bold")
+    //         .attr("class", "title")
+    //         .text(title));
+    
+    //     return svg.node();
+    // }
 }
